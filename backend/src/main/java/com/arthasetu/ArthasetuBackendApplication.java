7cf88e595c97a68165b7229dfd36ce90ef3f8062
srivathsajg <srivathsajg34@gmail.com>
2026-08-11 10:31:55 +0530

    feat: initialize Spring Boot backend

---
+++ backend/src/main/java/com/arthasetu/ArthasetuBackendApplication.java
@@ -0,0 +1,13 @@
+package com.arthasetu;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ArthasetuBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ArthasetuBackendApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/com/arthasetu/ArthasetuBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.arthasetu;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ArthasetuBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
